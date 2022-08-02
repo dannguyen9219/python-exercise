29b137f25bab2d2a3f923bc425e49b1a8621e33d
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-08-02 09:56:04 -0500

    lists completed

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,47 @@
-# A List is a collection which is ordered and changeable. Allows duplicate members.
+# A List is a collection which is ordered and changeable. Allows duplicate members. Like arrays in JS
+
+# Create list
+numbers = [1, 2, 3, 4, 5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# Use a constructor
+numbers2 = list((1, 2, 3, 4, 5))
+print(numbers, numbers2)
+
+# Get a value
+print(fruits[1])
+
+# Get length
+print(len(fruits))
+
+# Append to list
+fruits.append('Mangos')
+
+print(fruits)
+
+# Remove from list
+fruits.remove('Grapes')
+print(fruits)
+
+# Insert into position
+fruits.insert(2, 'Strawberries')
+print(fruits)
+
+# Change value
+fruits[0] = 'Blueberries'
+
+# Remove with pop
+fruits.pop(2)
+print(fruits)
+
+# Reverse list
+fruits.reverse()
+print(fruits)
+
+# Sort list
+fruits.sort()
+print(fruits)
+
+# Reverse sort
+fruits.sort(reverse=True)
+print(fruits)
