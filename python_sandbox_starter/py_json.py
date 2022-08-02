cde371161ff2b9882b3edbbeca5b07b07379adf7
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-08-02 12:15:04 -0500

    json incomplete

--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,11 @@
-# JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+# JSON is commonly used with data APIs. Here how we can parse JSON into a Python dictionary
+
+import json
+
+# Sample JSON
+userJSON = '{"first_name": "Dan", "last_name": "Nguyen", "age": 30}'
+
+# Parse to dict
+user = json.loads(userJSON)
+
+print(user)
